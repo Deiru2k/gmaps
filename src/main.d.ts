a0617cf9dd3a1cddbf38ef58a0cd34b670b03647
Dale <deiru2k@gmail.com>
2018-06-17 12:36:39 +0300

    module in declaration

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,10 +1,12 @@
-export interface DistanceResponse {
-  distance: number;
-  duration: number;
-}
-
-export default class GmapsClient {
-  constructor(API_KEY: string);
-
-  getDistance(origin: [string | number, string | number], destination: [string | number, string | number]): DistanceResponse;
+declare module "gmaps" {
+  export interface DistanceResponse {
+    distance: number;
+    duration: number;
+  }
+  
+  export default class GmapsClient {
+    constructor(API_KEY: string);
+  
+    getDistance(origin: [string | number, string | number], destination: [string | number, string | number]): DistanceResponse;
+  }
 }
