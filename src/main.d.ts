5a3d080e9852a15a4081803f0f563bf5d1aecc90
Dale <deiru2k@gmail.com>
2018-06-17 12:29:46 +0300

    fix: Proper declaration file

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,13 +1,5 @@
-declare module "gmaps" {
-  interface DistanceResponse {
-    distance: number;
-    duration: number;
-  }
-  namespace gmaps {
-    declare class GmapsClient {
-      constructor(API_KEY: string);
+export default class GmapsClient {
+  constructor(API_KEY: string);
 
-      getDistance(origin: [string | number, string | number], destination: [string | number, string | number]): DistanceResponse;
-    }
-  }
+  getDistance(origin: [string | number, string | number], destination: [string | number, string | number]): DistanceResponse;
 }
