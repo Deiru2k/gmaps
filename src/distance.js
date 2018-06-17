4ff0d855134e64522fda47aa80e66d5233be4721
Dale <deiru2k@gmail.com>
2018-06-17 13:07:58 +0300

    misc: remove log

--- src/distance.js
+++ src/distance.js
@@ -4,7 +4,6 @@ export function getDistance(apiClient, origins, destinations, mode = "driving") 
   return apiClient.distanceMatrix({ origins, destinations, mode, units: "metric" })
     .asPromise()
     .then((response) => {
-      console.log(response.json);
       const distance = _.get(response.json, 'rows.0.elements.0.distance.value');
       const duration = _.get(response.json, 'rows.0.elements.0.duration.value');
       
